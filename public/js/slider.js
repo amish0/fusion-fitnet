75d845bacdb5dc7c770cf87aa27736ba5ed74323
amish0 <amishkumar562@gmail.com>
2026-01-26 14:35:44 +0530

    moved all to backup

--- public/js/slider.js
+++
@@ -1,8 +0,0 @@
-let idx = 0;
-const slides = document.querySelectorAll("#slider img");
-
-setInterval(() => {
-  slides.forEach(i => i.style.display = "none");
-  slides[idx].style.display = "block";
-  idx = (idx + 1) % slides.length;
-}, 3000);
